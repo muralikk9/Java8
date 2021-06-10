083e78c968673e96e95b037310c4b041edb62238
mkodavati <murali_kodavati@intuit.com>
2021-06-10 15:03:36 +0530

    search changes

---
+++ src/com/java/algorithms/BinarySearch.java
@@ -0,0 +1,31 @@
+package com.java.algorithms;
+
+public class BinarySearch {
+
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int last = arr.length - 1;
+        int result = binarySearch(arr, 0, last, 50);
+        if (result == -1) {
+            System.out.println("search element not found in array");
+        } else {
+            System.out.println("search element found at index: " + result);
+        }
+    }
+
+    private static int binarySearch(int[] arr, int first, int last, int key) {
+       if(last >= first) {
+           int mid = first + (last-first)/2;
+           if(arr[mid] == key) {
+               return mid;
+           }
+           if(arr[mid] > key) {
+               return binarySearch(arr, first, mid-1, key);
+           } else {
+               return  binarySearch(arr, mid+1, last, key);
+           }
+        }
+        return -1;
+    }
+}
---
+++ src/com/java/algorithms/LinearSearch.java
@@ -0,0 +1,23 @@
+package com.java.algorithms;
+
+// complexity of the linear search is O(n)
+public class LinearSearch {
+
+    private static int search(int[] a, int x) {
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 10, 20};
+        int result = search(a, 10);
+        if(result == -1) {
+            System.out.println("search element not found");
+        } else {
+            System.out.println("search element found at : " + result);
+        }
+    }
+}
