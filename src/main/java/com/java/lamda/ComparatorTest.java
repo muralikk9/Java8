f747592692f49e584b44e35b2d559acc52d3e976
mkodavati <murali_kodavati@intuit.com>
2021-06-13 13:09:27 +0530

    code changes

--- src/main/java/com/java/lamda/ComparatorTest.java
+++ src/main/java/com/java/lamda/ComparatorTest.java
@@ -9,6 +9,7 @@ public class ComparatorTest {
         Comparator<Person> comparator = (a, b) -> a.getFirstName().compareTo(b.getLastName());
         Person p1 = new Person("abc", "cde");
         Person p2 = new Person("cde", "def");
-        comparator.compare(p1, p2);
+        int result = comparator.compare(p1, p2);
+        System.out.println("result :"  + result);
     }
 }
